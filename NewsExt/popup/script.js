16ca9a79f6d1619bdd37790aaa3b6cbc1c88440b
AStoerkel1 <astoer1@students.towson.edu>
2020-02-26 18:43:21 -0500

    added another line for a tenth link and testing commit

--- NewsExt/popup/script.js
+++ NewsExt/popup/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $.getJSON("https://api.nytimes.com/svc/news/v3/content/nyt/u.s..json?api-key=ajba3SMj5gAAjiZG7WjOXdJ3htyjVE1h", 
   function(data) {
     console.log(data);
-    for(var i =0; i <= 8; i++){
+    for(var i =0; i <= 9; i++){
       var title= data.results[i].title;
       var link= data.results[i].url;
       $("#title" + i).attr("href", link);
