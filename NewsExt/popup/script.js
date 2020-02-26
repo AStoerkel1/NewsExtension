26422a01c973fdbedf5a40d854578e22564b53e9
AStoerkel1 <41537999+AStoerkel1@users.noreply.github.com>
2020-02-26 17:59:05 -0500

    Initial commit

---
+++ NewsExt/popup/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $.getJSON("https://api.nytimes.com/svc/news/v3/content/nyt/u.s..json?api-key=ajba3SMj5gAAjiZG7WjOXdJ3htyjVE1h", 
+  function(data) {
+    console.log(data);
+    for(var i =0; i <= 8; i++){
+      var title= data.results[i].title;
+      var link= data.results[i].url;
+      $("#title" + i).attr("href", link);
+      $("#title" + i).text(title);
+    }
+
+  });
+});
