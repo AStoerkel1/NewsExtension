3c4aefbebab65696db63a8b51a9ffd7b981fbc05
AStoerkel1 <astoer1@students.towson.edu>
2020-03-15 00:32:06 -0400

    all you need to do is change pages :)))

--- NewsExt/popup/script.js
+++ NewsExt/popup/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $.getJSON("https://api.nytimes.com/svc/news/v3/content/nyt/u.s..json?api-key=ajba3SMj5gAAjiZG7WjOXdJ3htyjVE1h", 
+  $.getJSON("https://api.nytimes.com/svc/news/v3/content/nyt/science.json?api-key=ajba3SMj5gAAjiZG7WjOXdJ3htyjVE1h", 
   function(data) {
     console.log(data);
     for(var i =0; i <= 9; i++){
