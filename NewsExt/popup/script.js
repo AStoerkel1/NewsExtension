96da29154c768998cafe6310e721c29c87d4040a
AStoerkel1 <astoer1@students.towson.edu>
2020-03-15 18:56:30 -0400

    shit lookin good in the code now, added comments

--- NewsExt/popup/script.js
+++ NewsExt/popup/script.js
@@ -1,18 +1,43 @@
+
+
+/* wait until the document is ready */
 $(document).ready(function(){
+
+
+  /* then get data from the API */
   getData("U.S."),
+
+
+  /**
+   * when a button is clicked,
+   * get the class name and put that into the API URL
+   */
   $("button").click(function(){
-    var sec;
     $(this).attr("class", function(n, id){
-      sec = id;
-      getData(sec);
+      getData(id);
     })
   })
 });
 
+/**
+ * function to fetch the data from the API, 
+ * log it on the console and
+ * send that data to the page
+ */
 function getData(sec){
   $.getJSON("https://api.nytimes.com/svc/news/v3/content/nyt/" + sec.toLowerCase() + ".json?api-key=ajba3SMj5gAAjiZG7WjOXdJ3htyjVE1h",
   function(data) {
     console.log(data);
+    populate(data, sec);
+})
+};
+
+/**
+ * populates the page with the information
+ * from the API
+ */
+
+  function populate(data, sec){
     for(var i =0; i <= 9; i++){
       var title= data.results[i].title;
       var link= data.results[i].url;
@@ -22,5 +47,4 @@ function getData(sec){
       $("#image" + i).attr("src", photo);
       $("h1").text(sec);
     }
-  });
-}
+  }
